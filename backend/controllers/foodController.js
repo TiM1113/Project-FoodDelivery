f636152a7a4f2e3a19222f84a93625319de4a97a
TiM1113 <yuantian1113@gmail.com>
2024-11-23 23:39:21 +1030

    Finally solved the Post request with 'image' with terminal curl command

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,5 +1,5 @@
 // This is an API
-import foodModel from "../models/foodModel.js";
+import foodModel from '../models/foodModel.js';
 
 // importing the fs (file system) module in Node.js.import fs like this, it allows you to use file-related operations such as:
 // Reading files.
@@ -7,32 +7,37 @@ import foodModel from "../models/foodModel.js";
 // Deleting files.
 // Renaming files.
 // Creating directories, etc.
-import fs from 'fs'
+import fs from 'fs';
 
 // add food item (this is a controller)
-const addFood = async(req, res) => {
+const addFood = async (req, res) => {
 
-  
-// Get both filename and path
-  let image_filename = req.file ? `${req.file.filename}` : null;
+// Log the request file and body for debugging
+console.log(req.file); // To see if the file is received
+console.log(req.body); // To check if the other form data is received
 
-  const food = new foodModel({
-    name:req.body.name,
-    description:req.body.description,
-    price:req.body.price,
-    category:req.body.category,
-    image:`uploads/${req.file.filename}` // Store the full path instead of just filename
-  })
 
-  try{
-    await food.save();
-    res.json({success: true, message: "Food Added"})
-  }
-  catch(error){
-    console.log(error)
-    res.json({success:false, message:"Error"})
-  }
 
-}
 
-export {addFood}
+
+	// Get both filename and path
+
+	try {
+		let image_filename = `${req.file.filename}`;
+
+		const food = new foodModel({
+			name: req.body.name,
+			description: req.body.description,
+			price: req.body.price,
+			category: req.body.category,
+			image: image_filename, // Store the full path instead of just filename
+		});
+		await food.save();
+		res.json({success: true, message: 'Food Added'});
+	} catch (error) {
+		console.log(error);
+		res.json({success: false, message: 'Error'});
+	}
+};
+
+export {addFood};
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,33 @@
 // Create the basic express server
 // set up the express server
-import express from "express"
-import cors from "cors"
+import express from 'express';
+import cors from 'cors';
 // DB connection has been imported from config file
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-
-
-
+import {connectDB} from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
 
 // app config
-const app = express()
-const port = 4000
-
+const app = express();
+const port = 4000;
 
 // initialize the middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // DB connection
 connectDB();
 
 // API endpoints
-app.use("/api/food", foodRouter)
-
+app.use('/api/food', foodRouter);
 
 // use get() to request the data from the server
-app.get("/",(req, res)=>{
-  res.send("API Working")
-})
+app.get('/', (req, res) => {
+	res.send('API Working');
+});
 
 // to start the express server
-app.listen(port,()=>{
-  console.log(`Server Started on http://localhost:${port}`)
-})
+app.listen(port, () => {
+	console.log(`Server Started on http://localhost:${port}`);
+});
 
 // mongodb+srv://yuantian1113:Yt7517299879@cluster0.llnlj.mongodb.net/?
