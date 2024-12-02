72ea52a780de0e554a917592c32f5ea9936119df
TiM1113 <yuantian1113@gmail.com>
2024-12-02 14:07:48 +1030

    finished user register and login API in useController componment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,8 +8,27 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import validator from 'validator';
 
-// login user
-const loginUser = async (req, res) => {};
+// the logic for login user
+const loginUser = async (req, res) => {
+	const {email, password} = req.body;
+	try {
+		const user = await userModel.findOne({email});
+		if (!user) {
+			return res.json({success: false, message: "User Doesn't exist"});
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return res.json({success: false, message: 'Invalid credentials'});
+		}
+
+		const token = createToken(user._id);
+		res.json({success: true, token});
+	} catch (error) {
+		console.log(error);
+		res.json({success: false, message: 'Error'});
+	}
+};
 
 // create a function to create token for new users
 const createToken = (id) => {
