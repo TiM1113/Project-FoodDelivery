8d9179060a200c88f6304b83d94181ff1900626c
TiM1113 <yuantian1113@gmail.com>
2024-12-12 23:43:07 +1030

    Completed removeFromCart route handlers function in cartController.js

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -5,7 +5,7 @@ import userModel from '../models/userModel.js';
 const addToCart = async (req, res) => {
   try {
     // to find the user data
-    let userData = await userModel.findOne({_id:req.body.userId});
+    let userData = await userModel.findById(req.body.userId);
     let cartData = await userData.cartData;
     if (!cartData[req.body.itemId]) {
       cartData[req.body.itemId] = 1; // first time: it will execute this statement to add item to cart
@@ -22,7 +22,19 @@ const addToCart = async (req, res) => {
 
 // 2- remove items from user cart
 const removeFromCart = async (req, res) => {
-
+  try {
+    let userData = await userModel.findById(req.body.userId) // find the userId by auth.js middleware - decode token and convert it into userId
+    // extract the cart data
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+    res.json({success:true, message:"Removed From Cart"})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:"Error"})
+  }
 };
 
 // 3- fetch user cart data
