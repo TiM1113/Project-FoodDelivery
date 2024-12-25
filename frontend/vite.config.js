7aec71936ed783a1bda742e548e3a8c262d223d3
TiM1113 <yuantian1113@gmail.com>
2024-12-26 01:39:10 +1030

    Update frontend before rebuilding dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Adjust this based on your deployment structure
+});
